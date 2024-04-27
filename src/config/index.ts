e5c362323e9c6bf8726027a32d7d32b9c243170a
Hemal99 <h.githmin@gmail.com>
2024-04-27 19:08:24 +0530

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const MONGO_URI = `mongodb+srv://hgithmin:${process.env.MONGO_PASSWORD}@cluster0.9xb6vu5.mongodb.net/`;
+export const MONGO_URI = `mongodb+srv://omsahrar:${process.env.MONGO_PASSWORD}@cluster0.6dpisy0.mongodb.net/`;
 export const APP_SECRET = "238745623hsdf";
 
 export const PORT = process.env.PORT || 5000;
